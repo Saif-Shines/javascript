33e0d91f1720dd1fce32f734a4dd341849ce8b44
Saif Ali Shaik <saifshine7@gmail.com>
2020-05-03 13:49:29 +0530

    ch 15

--- closures/solutions.js
+++ closures/solutions.js
@@ -281,15 +281,29 @@ myNewFunc2(); // => 1
 myNewFunc2(); // => 2
 
 // CHALLENGE 15
-function russianRoulette(num) {}
+function russianRoulette(num) {
+  let limit = num;
+  let count = 0;
+  function playGame() {
+    count++;
+    if (count < num) {
+      return 'click'
+    } else if (count == num) {
+      return 'bang'
+    } else {
+      return 'reload to play again'
+    }
+  }
+  return playGame;
+}
 
-// /*** Uncomment these to check your work! ***/
-// const play = russianRoulette(3);
-// console.log(play()); // => should log 'click'
-// console.log(play()); // => should log 'click'
-// console.log(play()); // => should log 'bang'
-// console.log(play()); // => should log 'reload to play again'
-// console.log(play()); // => should log 'reload to play again'
+/*** Uncomment these to check your work! ***/
+const play = russianRoulette(3);
+console.log(play()); // => should log 'click'
+console.log(play()); // => should log 'click'
+console.log(play()); // => should log 'bang'
+console.log(play()); // => should log 'reload to play again'
+console.log(play()); // => should log 'reload to play again'
 
 // CHALLENGE 16
 function average() {}
