70880f00616320bd39d15a3117b70d6646ee01d4
Saif Ali Shaik <saifshine7@gmail.com>
2020-05-03 08:47:45 +0530

    challenge 8 complete

--- closures/solutions.js
+++ closures/solutions.js
@@ -156,15 +156,25 @@ rollCaller() // => should log 'Everyone accounted for'
 
 // CHALLENGE 8
 function saveOutput(func, magicWord) {
-
+  let Obj = {};
+  let callback = func;
+  let pass = magicWord;
+  function addToObj(x) {
+    if (pass == x) {
+      return Obj;
+    }
+    Obj[x] = callback(x);
+    return callback(x);
+  }
+  return addToObj;
 }
 
-// /*** Uncomment these to check your work! ***/
-// const multiplyBy2 = function(num) { return num * 2; };
-// const multBy2AndLog = saveOutput(multiplyBy2, 'boo');
-// console.log(multBy2AndLog(2)); // => should log 4
-// console.log(multBy2AndLog(9)); // => should log 18
-// console.log(multBy2AndLog('boo')); // => should log { 2: 4, 9: 18 }
+/*** Uncomment these to check your work! ***/
+const multiplyBy2 = function (num) { return num * 2; };
+const multBy2AndLog = saveOutput(multiplyBy2, 'boo');
+console.log(multBy2AndLog(2)); // => should log 4
+console.log(multBy2AndLog(9)); // => should log 18
+console.log(multBy2AndLog('boo')); // => should log { 2: 4, 9: 18 }
 
 
 // CHALLENGE 9
