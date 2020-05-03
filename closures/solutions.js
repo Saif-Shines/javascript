8b5ccaa6313e27baac4b7f84c92fd342f436f442
Saif Ali Shaik <saifshine7@gmail.com>
2020-05-03 09:40:02 +0530

    ch 10 complete

--- closures/solutions.js
+++ closures/solutions.js
@@ -203,13 +203,16 @@ console.log(getDay()); // => should log 'Fri'
 
 // CHALLENGE 10
 function defineFirstArg(func, arg) {
-
+  function resFunc(b) {
+    return func(arg, b);
+  }
+  return resFunc;
 }
 
-// /*** Uncomment these to check your work! ***/
-// const subtract = function(big, small) { return big - small; };
-// const subFrom20 = defineFirstArg(subtract, 20);
-// console.log(subFrom20(5)); // => should log 15
+/*** Uncomment these to check your work! ***/
+const subtract = function (big, small) { return big - small; };
+const subFrom20 = defineFirstArg(subtract, 20);
+console.log(subFrom20(5)); // => should log 15
 
 
 // CHALLENGE 11
